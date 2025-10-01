36f9120eb6eb6a4556a4599576c5e3f60285c7c5
GitEnMain <myckael.prof.s2i@gmail.com>
2025-10-01 16:09:37 +0200

    W majuscule

--- docs/script.js
+++ docs/script.js
@@ -70,7 +70,7 @@ const students = [
     role: "Développeur Java & Spigot",
     email: "the-agag@exemple.com",
     github: "https://github.com/THE-AGAG",
-    web: "pages/Processeur/processeur.html",
+    Web: "pages/Processeur/processeur.html",
     tech: ["Java", "Spigot", "Bukkit", "Maven", "Git"],
     bio: "Créateur de plugins / Mods Minecraft."
   },
