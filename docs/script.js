a4ede0d819207ed4712c7993b9681f759dc3064c
anto <antonin.lespagnol@etud.u-picardie.fr>
2025-10-03 16:19:56 +0200

    chgmnt_bio2

--- docs/script.js
+++ docs/script.js
@@ -94,7 +94,7 @@ const students = [
     github: "https://github.com/antonin-upjv",
     web: "pages/Apple_M4_SoC/Apple_M4_index.html",
     tech: ["UX", "UI", "Pixelmator Pro"],
-    bio: "Ce n’est pas n’importe qui qui peut devenir un grand artiste, mais un grand artiste peut venir de n’importe où."
+    bio: "~Tout le monde peut cuisiner.~ Gusteau"
   },
   {
     name: "Mamadou Mams05",
