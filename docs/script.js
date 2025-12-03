0242e605725803bbc3994f11834a33f1a952ecac
THE-AGAG <lengletgaetan13@gmail.com>
2025-12-03 21:18:04 +0100

    Update web link in script.js

--- docs/script.js
+++ docs/script.js
@@ -73,7 +73,7 @@ const students = [
     role: "Développeur Java & Spigot",
     email: "the-agag@exemple.com",
     github: "https://github.com/THE-AGAG",
-    web: "riskAtWork/Lenglet Gaëtan/Risk at Work.html",
+    web: "risksAtWork/Students_Works/Lenglet Gaëtan",
     tech: ["Java", "Spigot", "Bukkit", "Maven", "Git"],
     bio: "Créateur de plugins / Mods Minecraft."
   },
